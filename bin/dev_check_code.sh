07926cec737cd2584ab85478191c1e166bd588fe
Joanis Rouanet <joanis.ang3@gmail.com>
2022-02-20 15:16:29 +0100

    Update dev_check_code.sh

--- bin/dev_check_code.sh
+++ bin/dev_check_code.sh
@@ -8,34 +8,16 @@ echo
 
 if [ -z "$1" ]
 then
-	directory='src'
-	echo "No directory specified (default: src)."
+	level=9
+	echo "No level specified (default: 9 [max])."
 	echo 
 else
-	directory=$1
+	level=$1
 	echo "Level:" $1
 	echo 
-fi
-
-if [ -z "$2" ]
-then
-	level=7
-	echo "No level specified (default: 7 [max])."
-	echo 
-else
-	level=$2
-	echo "Level:" $2
-	echo 
-fi
-
-if [ $directory = "src" ]
-then
-    config_file='phpstan.neon'
-else
-    config_file='phpstan.'$directory'.neon'
 fi
 
 echo "Config file:" $config_file
 echo 
 
-phpstan analyse $directory -c $config_file -l $level
+phpstan analyse $directory -c phpstan.neon -l $level
